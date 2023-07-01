8d5403cac5e5b2fc78ab62f74c2732bbc73f136b
likurg42 <likurg42@gmail.com>
2023-07-01 06:25:28 +0300

    feat: add global styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,5 @@
+const colors: { [color: string]: string } = {
+  primary: '#45a9da',
+};
+
+export default colors;
---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+import GlobalStyles from '@styles/GlobalStyles.tsx';
+import colors from '@styles/colors.ts';
+
+export const HEADER_HEIGHT = 60;
+export const FOOTER_HEIGHT = 36;
+
+export const SECOND_IN_MS = (seconds: number) => 1000 * seconds;
+
+export const Z_INDEX_LEVEL_1 = 1;
+export const Z_INDEX_LEVEL_2 = 19;
+export const Z_INDEX_LEVEL_3 = 100;
+export const Z_INDEX_LEVEL_4 = 1000;
+export const Z_INDEX_LEVEL_5 = 10000;
+
+export { GlobalStyles, colors };
