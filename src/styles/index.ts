c025109fedff43d3676f8b1ef5a300ebdecbb94a
likurg42 <likurg42@gmail.com>
2023-07-03 20:05:36 +0300

    refactor: take out css reset in separate file

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-import GlobalStyles from '@styles/globalStyles.tsx';
+import GlobalStyles from '@styles/global-styles.tsx';
 import BaseTheme from '@styles/themes/base-theme.ts';
 
 export { BaseTheme, GlobalStyles };
